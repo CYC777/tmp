729edab4340f2926ddfdf9d0dcceaa7577e8df52
CYC777 <travisingd@gmail.com>
2018-10-26 22:19:33 -0400

    oauth redirect error

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,9 @@
+
+
+export default function(state = null, action) {
+  console.log(action);
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
--- services/passport.js
+++ services/passport.js
@@ -6,38 +6,37 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-    User.findById(id)
-        .then(user => {
-            done(null, user);
-        });
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
 });
 
 passport.use(new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback',
-            proxy: true
-        },
-        (accessToken, refreshToken, profile, done) => {
-            console.log('access token', accessToken);
-            console.log('refresh token', refreshToken);
-            console.log('profile', profile);
+  {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+    proxy: true
+  },
+  async (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refresh token', refreshToken);
+      console.log('profile', profile);
 
-            User.findOne({googleId: profile.id})
-                .then((existingUser) => {
-                    if (existingUser) {
-                        done(null, existingUser);
-                    } else {
-                        new User({ googleId: profile.id }).save()
-                            .then(user => done(null, user));
-                    }
+      const existingUser = await User.findOne({googleId: profile.id});
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
 
-                });
-        }
-    )
+
+    }
+  )
 );
